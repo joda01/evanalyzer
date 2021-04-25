342f02eaf542efd020c2634ca60ecaf82589de54
Joachim Danmayr <joachim@danmayr.org>
2021-04-25 17:36:37 +0000

    #23 New coloc method
    resolves #23

--- src/org/danmayr/imagej/Version.java
+++ src/org/danmayr/imagej/Version.java
@@ -2,8 +2,8 @@ package org.danmayr.imagej;
 
 public class Version {
     static String major = "2";
-    static String minor = "9";
-    static String fix = "1";
+    static String minor = "10";
+    static String fix = "0";
     static String status = "alpha";
     static long build  = 1618161635;
 
