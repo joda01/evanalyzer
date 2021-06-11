259896d299682a6b5b70369db45daa58196b0264
Joachim Danmayr <joachim@danmayr.org>
2021-06-11 08:32:40 +0000

    Created jar file

--- src/org/danmayr/imagej/Version.java
+++ src/org/danmayr/imagej/Version.java
@@ -3,7 +3,7 @@ package org.danmayr.imagej;
 public class Version {
     static String major = "3";
     static String minor = "0";
-    static String fix = "6";
+    static String fix = "7";
     static String status = "alpha";
     static long build  = 1622055908;
 
