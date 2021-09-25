ef04ae8dd62d45622f2477226ea0650f82953678
Joachim Danmayr <joachim@danmayr.org>
2021-09-25 19:24:42 +0000

    Fixed lines of code

--- src/org/danmayr/imagej/gui/NewsTickerText.java
+++ src/org/danmayr/imagej/gui/NewsTickerText.java
@@ -12,7 +12,7 @@ public class NewsTickerText {
         "In year 2020 around 16'000'000 webpages were online",
         "Quantum computers will be able to crack all asymmetric encryption methods used so far.",
         "Googles search index containes 8'058'044'651 documents in 2004, 130'000'000'000'000 in 2020",
-        "This plugin consists of more than 2400 lines of code.",
+        "This plugin consists of more than 6200 lines of code.",
         "The open source LINUX kernel has more than 20'000 contributors from 2002 to 2019.",
         "The bases of the nucleic acids are called adenine (A), cytosine (C), guanine (G), thymine (T) and uracil (U)",
         "OECD fields of Natural Science are Mathematics, Computer-, Physical-, Chemical-, Biological-, Earth & related environmental sciences.",
