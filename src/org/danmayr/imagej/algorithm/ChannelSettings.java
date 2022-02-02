eca866ebe1291471220077f884321d57faba543c
Joachim Danmayr <joachim@danmayr.org>
2022-02-02 18:47:06 +0000

    Fixed min circularity Closes #75

--- src/org/danmayr/imagej/algorithm/ChannelSettings.java
+++ src/org/danmayr/imagej/algorithm/ChannelSettings.java
@@ -71,7 +71,7 @@ public class ChannelSettings implements Cloneable {
     }
 
     public void setMinCircularityDouble(double v) {
-        mMinParticleSize = v;
+        mMinCircularity = v;
     }
 
     public void setMinParticleSizeDouble(double v) {
