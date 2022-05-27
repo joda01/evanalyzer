dd7f993bd34a5119224b5ef03f190a1a863ff307
Joachim Danmayr <46405460+joda01@users.noreply.github.com>
2022-05-27 16:25:38 +0200

    Support ICS format

--- src/org/danmayr/imagej/algorithm/FileProcessor.java
+++ src/org/danmayr/imagej/algorithm/FileProcessor.java
@@ -254,6 +254,7 @@ public class FileProcessor extends Thread {
                 } else if (file.getName().toLowerCase().endsWith(".vsi")
                         || file.getName().toLowerCase().endsWith(".tiff")
                         || file.getName().toLowerCase().endsWith(".czi")
+                        || file.getName().toLowerCase().endsWith(".ics")
                         || file.getName().toLowerCase().endsWith(".tif")) {
                     foundFiles.add(file);
                 }
